21f5b864c63dfa836a6598cfe8cf2dcf397467b6
crknuchu <mihailo.bogdanovic1@gmail.com>
2022-11-17 22:51:19 +0100

    implement hover events

--- Yugioh/headers/Card.h
+++ Yugioh/headers/Card.h
@@ -3,17 +3,21 @@
 
 #include <QGraphicsPixmapItem>
 #include <QGraphicsItem>
+#include <QGraphicsSceneHoverEvent>
 
 
 class Card: public QGraphicsPixmapItem {
     public:
         Card(QGraphicsItem *parent = 0);
 
-        void pickCard();
+        void menuPopUp(QGraphicsSceneHoverEvent *event);
         void mousePressEvent(QGraphicsSceneMouseEvent*event);
         void setName(std::string name);
         float getHeight();
         float getWidth();
+        void hoverEnterEvent(QGraphicsSceneHoverEvent *event);
+        void hoverLeaveEvent(QGraphicsSceneHoverEvent *event);
+        void hoverMoveEvent(QGraphicsSceneHoverEvent *event);
     private:
         std::string cardName = "";
         std::string owner = "";
--- Yugioh/sources/Card.cpp
+++ Yugioh/sources/Card.cpp
@@ -7,21 +7,37 @@
 
 Card::Card(QGraphicsItem *parent){
     //set graphics
-    QPixmap pixmap(":/resources/card_back.jpg");
-    
+    QPixmap pixmap(":/resources/blue_eyes.jpg");
     pixmap = pixmap.scaled(QSize(200,150),Qt::KeepAspectRatio); //pixmap size needs to not be hardcoded
     height = pixmap.height();
     width = pixmap.width();
     setPixmap(pixmap);
+    setAcceptHoverEvents(true);
 }
 
 void Card::mousePressEvent(QGraphicsSceneMouseEvent *event){
-    pickCard();
-    //TODO: implement menu that pops up from card with options for player to pick
+    if(event->button() == Qt::LeftButton){
+        // std::cout<<"left"<<std::endl;
+    }
 }
 
-void Card::pickCard(){
-    std::cout<<cardName<<std::endl;
+void Card::hoverEnterEvent(QGraphicsSceneHoverEvent *event){
+
+    // if(event->type() == QEvent::HoverEnter){
+    // std::cout<<"enter"<<std::endl;
+    menuPopUp(event);
+}
+
+void Card::hoverLeaveEvent(QGraphicsSceneHoverEvent *event){
+    std::cout<<"leave"<<std::endl;
+}
+
+void Card::hoverMoveEvent(QGraphicsSceneHoverEvent *event){
+    //std::cout<<"move"<<std::endl;
+}
+
+void Card::menuPopUp(QGraphicsSceneHoverEvent *event){
+    std::cout<<"menu pops up"<<std::endl;
 }
 
 void Card::setName(std::string name){
