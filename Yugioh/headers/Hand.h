0372fcba4ffe9f8f46313c2ba5ba28948d0f5313
jovan <jovanskoric02@gmail.com>
2022-11-19 19:32:25 +0100

    feat:Implement add and remove functions for hand

--- Yugioh/headers/Hand.h
+++ Yugioh/headers/Hand.h
@@ -1,11 +1,15 @@
 #ifndef HAND_H
 #define HAND_H
 
-#import "CardList.h"
+#include "CardList.h"
+#include <optional>
 
 class Hand : public CardList {
 public:
-    Hand(std::vector<Card> &initialHand = [])
+    Hand(std::vector<Card> &initialHand);
+    std::vector<Card> getHand() const;
+    void addToHand(Card &card);
+    std::optional<Card> removeFromHand(Card &card);
 };
 
 #endif // HAND_H
--- Yugioh/sources/Hand.cpp
+++ Yugioh/sources/Hand.cpp
@@ -1,4 +1,28 @@
-#import "headers/Hand.h"
+#include "headers/Hand.h"
+#include <iostream>
+#include <algorithm>
+#include <optional>
 
 Hand::Hand(std::vector<Card> &initialHand)
-    :CardList(initialHand){}
+    :CardList(initialHand){
+    for(auto x : m_cardList){
+        std::cout << x.name() << " " ;
+    }
+}
+
+std::vector<Card> Hand::getHand() const{
+    return m_cardList;
+}
+
+void Hand::addToHand(Card &card) {
+    m_cardList.push_back(card);
+}
+
+std::optional<Card> Hand::removeFromHand(Card &card) {
+    std::vector<Card>::iterator it = std::find(m_cardList.begin(), m_cardList.end(), card);
+    if(it == m_cardList.end())
+        return {};
+
+     m_cardList.erase(it);
+     return card;
+}
