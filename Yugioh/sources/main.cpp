674875db0e151482b4c14ba3797f3f086d81e39b
jovan <jovanskoric02@gmail.com>
2022-11-19 19:41:08 +0100

    test:Test classes methods

--- Yugioh/sources/main.cpp
+++ Yugioh/sources/main.cpp
@@ -1,4 +1,34 @@
+#include "headers/Card.h"
+#include "headers/Hand.h"
+#include "headers/Deck.h"
+#include "headers/Graveyard.h"
+#include <iostream>
+
 int main()
 {
-    return 0;
+    Card card1 = Card("plavac", "plavi zmaj");
+    Card card2 = Card("zmaj", "crni zmaj");
+    Card card3 = Card("okut", "beli zmaj");
+    std::vector<Card> c;
+//    c.push_back(card1);
+//    c.push_back(card2);
+//    c.push_back(card1);
+//    std::cout << "hand:" << std::endl;
+//    Hand h = Hand(c);
+//    std::cout << "deck:" << std::endl;
+//    c.push_back(card3);
+//    c.push_back(card3);
+//    Deck d = Deck("hello",c);
+
+//    std::cout << "grave:" << std::endl;
+    Graveyard g = Graveyard(c);
+//    std::vector<Card> d_copy = d.getDeck();
+//    std::vector<Card> drawnCards = d.draw(2);
+
+//    d.shuffleDeck();
+    g.sendToGraveyard(card1);
+    g.sendToGraveyard(card2);
+
+    std::optional<Card> cardFromGY  = g.removeFromGraveyard(card1);
+    std::cout << cardFromGY->name();
 }
