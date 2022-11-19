a6aa6d998e484fd409359bdb55f2c1580447198e
jovan <jovanskoric02@gmail.com>
2022-11-19 19:21:00 +0100

    feat:Implement add and remove functions for graveyard

--- Yugioh/headers/Graveyard.h
+++ Yugioh/headers/Graveyard.h
@@ -1,4 +1,15 @@
 #ifndef GRAVEYARD_H
 #define GRAVEYARD_H
 
+#include "CardList.h"
+#include <optional>
+
+class Graveyard : public CardList {
+public:
+    Graveyard(std::vector<Card> &initialGraveyard);
+    void sendToGraveyard(const Card &card);
+    std::vector<Card> getGraveyard() const;
+    std::optional<Card>  removeFromGraveyard(const Card &card);
+};
+
 #endif // GRAVEYARD_H
