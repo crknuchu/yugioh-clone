39ecb40cfbb2255f4ce5c8fd4db85d91a6069760
fongsd <mi18293@alas.matf.bg.ac.rs>
2022-12-08 12:06:20 +0100

    feat: new functions for phases
    refactor code, set phase parts and put functions in appropriate parts

--- Yugioh/headers/Player.h
+++ Yugioh/headers/Player.h
@@ -5,6 +5,8 @@
 #include <iostream>
 #include <memory>
 #include "headers/Monstercard.h"
+#include "headers/Spellcard.h"
+#include "headers/Trapcard.h"
 #include "headers/Game.h"
 #include "Hand.h"
 
@@ -18,21 +20,52 @@ public:
 
   };
   Player(std::string playerName, int points = 8000) : m_name(playerName), m_points(points){};
-
-  Card &putCardOnTheTable(Hand &, Game &);
+  //DRAW PHASE
   void drawCards(unsigned int numOfCards);
+  void activationSpellCard();
+  void activationTrapCard();
+  // ------------------------------------------
+
+  //STANDBYPHASE
+  void automaticallyActivationSBPhase();
+  // ------------------------------------------
+
+  //MAIN PHASE 1 
+  Card &putCardOnTheTable(Hand &, Game &);
+  void addMonsterCardOnTable(MonsterCard&);//activate this method whenever add new monsterCard on ground
+  void automaticallyActivationMPhase();
+  void setCardPosition();
+  // ------------------------------------------
+
+  //BATTLE PHASE
+  int checkOpponentGround(const Player &opponent) const;
+  std::vector<MonsterCard *> tableMonsterCards(const Player &opponent); //check vector size before attack
   void attackOpponent(Game &game, MonsterCard a, Player &opponent);
+
+  // -----------------------------------------
+
+  //MAIN PHASE 2 -> same as MAIN PHASE 1 + XYZ Summon
+  void XYZSummon();
+  // -----------------------------------------
+
+  //END PHASE
+
+  //TODO
+  // add missing methods
+  
+  //
+
+
   std::string getPlayerName() const;
   unsigned getPlayerPoints();
   void setPoints(unsigned points);
   bool operator==(const Player &other) const; // a == b // In our case, a == *this, b == other
-  int checkOpponentGround(const Player &opponent) const;
-  void addMonsterCardOnTable(MonsterCard&);//activate this method whenever add new monsterCard on ground
-  std::vector<MonsterCard *> tableMonsterCards(const Player &opponent); //check vector size before attack
 private:
     std::string m_name;
     unsigned m_points;
     std::vector<MonsterCard *> m_tableMonsterCards; //vector of monsterCards on the table, so i can check if opponent has any monsterCard on the table in order to attack him
+    std::vector<SpellCard *>m_tableSpellCards;//live spell cards
+    std::vector<TrapCard *>m_tableTrapCards;//live trap cards
 };
 
 // std::istream &operator>>(std::istream &in, Player &p);
