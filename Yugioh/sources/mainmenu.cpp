df916865a657cfa89b8414a4c585b9d0b42bc440
Darija <darijae99@gmail.com>
2022-12-17 21:05:25 +0100

    Add cardmenu.cpp

---
+++ Yugioh/sources/mainmenu.cpp
@@ -0,0 +1,75 @@
+#include "headers/mainmenu.h"
+#include "ui_mainmenu.h"
+#include "headers/Game.h"
+#include "headers/Player.h"
+#include "headers/GameSettings.h"
+#include <QDir>
+#include <QScreen>
+#include <QMediaPlayer>
+
+
+MainMenu::MainMenu(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainMenu)
+{
+    ui->setupUi(this);
+    QPixmap bkgnd(":/resources/pictures/menubackground.png");
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    int height = screenGeometry.height();
+    int width = screenGeometry.width();
+
+    bkgnd = bkgnd.scaled(width,height, Qt::KeepAspectRatioByExpanding);
+    QPalette palette;
+    palette.setBrush(QPalette::Window, bkgnd);
+    this->setPalette(palette);
+    QMediaPlayer * music = new QMediaPlayer();
+    music->setMedia(QUrl("qrc:/resources/sounds/illusion.mp3"));
+    music->play();
+
+
+}
+
+MainMenu::~MainMenu()
+{
+    delete ui;
+}
+
+void MainMenu::on_pushButton_clicked()
+{
+
+    //Player player1("Nikola");
+    //Player player2("Milan");
+    //game = new Game(player1,player2);
+    //Game game(player1, player2);
+    //game->show();
+}
+
+
+void MainMenu::on_pushButton_4_clicked()
+{
+    close();
+}
+
+
+void MainMenu::on_pushButton_5_clicked()
+{
+    gameSettings = new GameSettings();
+    gameSettings->show();
+
+}
+
+
+
+void MainMenu::setGame(Game *newGame)
+{
+    game = newGame;
+}
+
+
+
+
+
+
+
+
